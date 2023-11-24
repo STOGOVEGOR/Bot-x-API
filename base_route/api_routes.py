7ebcdeebae2f58c5d62335f6e3bcf4dfa3f9c241
Stogov Egor <severeff@gmail.com>
2023-11-24 07:29:54 +0100

    add get_bots route

--- base_route/api_routes.py
+++ base_route/api_routes.py
@@ -28,11 +28,18 @@ class ReverifyEmailRoute(BaseRoute):
 
 class BotList(BaseRoute):
     def prepare_response(self, response):
-        res = json.loads(response.content)
+        res = json.loads(response._content)
         for item in res['result']:
-            del(item["available"])
-            del(item["status_code"])
-            item["model"] = "Bot"
+            # del(item["available"])
+            # del(item["status_code"])
+            item["code"] = item["name"]
+            item["model_name"] = None
+            item["mode_name"] = None
+            if 'gpt' in item["name"]:
+                tmp_list = item["name"].split('-')
+                item["name"] = 'ChatGPT'
+                item["model_name"] = f"{tmp_list[0].capitalize()}-{tmp_list[1]}"
+                item["mode_name"] = f"{tmp_list[-1]} context"
 
         response._content = json.dumps(res)
         super().prepare_response(response)
